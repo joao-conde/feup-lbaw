eacf3153643e419a9fe8612fef7370b92805acdc
leonardomgt <leogt-15@hotmail.com>
2018-02-27 18:15:14 +0000

    profile page updates

--- src/profile.php
+++ src/profile.php
@@ -1,46 +1,121 @@
 
 <?php  include_once('templates/header.php'); ?>
 
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <link rel="stylesheet" href="styles/profile.css">
-    <div class="profile-page row">
+    <div class="profile-page row m-0" style="width: 100%">
         <div class="background-image"></div>
-        <div class="card float-right profile-card col-4" style="width:300px">
-            <img class="card-img-top" src="images/users/example-user.jpg" alt="Card image" style="width:100%">
+        <div class="card float-right profile-card col-3 ml-2">
+            <img class="card-img-top mt-2" src="images/users/example-user.jpg" alt="Card image" style="width:100%">
             <div class="card-body">
                 <div class="row">
                     <h4 class="card-title col-sm-10">Leonardo Teixeira</h4>
                     <img class="px-2 pb-2 edit-icon" src="images/system/edit-icon.svg" alt="Edit" width="36" height="36">
                 </div>
-                <!-- <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p> -->
-                <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
 
-                <div class="panel-group">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title btn btn-primary">
-                                <a id="skills" data-toggle="collapse" href="#collapse1">Skills and Instruments</a>
-                            </h4>
-                        </div>
-                        <div id="collapse1" class="panel-collapse collapse">
-                            <ul class="list-group">
-                            <li class="list-group-item">Guitar</li>
-                            <li class="list-group-item">Drums</li>
-                            <li class="list-group-item">Bass</li>
-                            <li class="list-group-item">Vocals</li>
-                            </ul>
-                        </div>
-                    </div>
+
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">Skills</a>
+                    <a class="list-group-item" href="#">
+                        <span class="list-group-item-text">Drums</span>
+
+                        <small class="mt-1 ml-1" style="float: right">4.2</small>
+                        <i class="far fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                    </a>
+                    <a class="list-group-item" href="#">
+                        <span class="list-group-item-text">Guitar</span>
+                        <small class="mt-1 ml-1" style="float: right">3.1</small>
+                        <i class="far fa-star mt-1" style="float: right"></i>
+                        <i class="far fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                    </a>
+                    <a class="list-group-item" href="#">
+                        <span class="list-group-item-text">Piano</span>
+                        <small class="mt-1 ml-1" style="float: right">2.9</small>
+                        <i class="far fa-star mt-1" style="float: right"></i>
+                        <i class="far fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                        <i class="fas fa-star mt-1" style="float: right"></i>
+                    </a>
                 </div>
             </div>
         </div>
 
-        <!-- <div class="container">
-            <div class="jumbotron col-4">
-                <h3>Some post text</h3>
+        <div class="container col m-0">
+
+            <div class="jumbotron bg-white pt-4 pb-4 mt-2 mb-0">
+                <div class="row mb-3">
+                    <img class="user-post-img rounded-circle" src="images/users/example-user.jpg" alt="Profile picture">
+                    <h4 class="col-10 pt-2">Leonardo Teixeira</h4>
+                </div>
+                <img class="post-img" src="images/users/example-post.jpg" alt="Post picture">
+            </div>
+
+            <div class="jumbotron bg-white pt-4 pb-4 mt-2 mb-0">
+                <div class="row mb-3">
+                    <img class="user-post-img rounded-circle" src="images/users/example-user.jpg" alt="Profile picture">
+                    <h4 class="col-10 pt-2">Leonardo Teixeira</h4>
+                </div>
+                <img class="post-img" src="images/users/example-post.jpg" alt="Post picture">
+            </div>
+        </div>
+
+        <div class="container col-3 pr-0">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="carousel-img d-block w-100" src="images/users/example-post.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="carousel-img d-block w-100" src="images/users/example-post2.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="carousel-img d-block w-100" src="images/users/example-post3.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+
+        <!-- <div class="container col m-0">
+
+            <div class="jumbotron bg-white pt-4 pb-4 mt-2 mb-0">
+                <div class="row mb-3">
+                    <img class="user-post-img rounded-circle" src="images/users/example-user.jpg" alt="Profile picture">
+                    <h4 class="col-10 pt-2">Leonardo Teixeira</h4>
+                </div>
+                <img class="post-img" src="images/users/example-post.jpg" alt="Post picture">
+            </div>
+
+            <div class="jumbotron bg-white pt-4 pb-4 mt-2 mb-0">
+                <div class="row mb-3">
+                    <img class="user-post-img rounded-circle" src="images/users/example-user.jpg" alt="Profile picture">
+                    <h4 class="col-10 pt-2">Leonardo Teixeira</h4>
+                </div>
+                <img class="post-img" src="images/users/example-post.jpg" alt="Post picture">
             </div>
         </div> -->
+
     </div>
 
 </body>
 
-</html>
+</html> 
+
+
+ <!-- <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p> -->
+                <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
