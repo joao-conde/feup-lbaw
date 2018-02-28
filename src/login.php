7b368b302979e76f128d39bd52ccd98e494c2c34
Joao Furriel <joaofurriel@wifi-interno.fe.up.pt>
2018-02-28 14:34:42 +0000

    header ready

--- src/login.php
+++ src/login.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/flatly/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="styles/logincss.css">
-</head>
-<body>
+<link rel="stylesheet" type="text/css" href="styles/logincss.css">
+
+<?php include('templates/header.php'); ?>
 
 <div class="container-fluid">
 	<div class="row justify-content-center">
 		<div class="col-sm-4">
-		<div class="jumbotron">
+		<div class="jumbotron text-center">
 		  	<h2>Login</h2>
 		  	<form action="/action_page.php">
 		    	<div class="form-group">
