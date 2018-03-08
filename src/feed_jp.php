994eb07a95dd195a58e8c80b3a9c2f20cb1d1ea3
therealdelay <danny.a_soares@hotmail.com>
2018-03-08 00:18:23 +0000

    feed

--- src/feed_jp.php
+++ src/feed_jp.php
@@ -79,59 +79,23 @@
 
         <div id="posts" class="col-12 col-md-6 p-0 mt-2">
 
-            <div class="row p-2 m-0 bg-primary d-block d-md-none">
-                <h6 class="col-auto m-0 text-success">New Post</h6>
-            </div>
 
-            <div id="new_post" class="jumbotron py-2 mb-3">
-                <div class="row justify-content-between p-0">
-
-                    <div class="col-3 col-md-4 align-self-center">
-                        <i>New Post</i>
-                    </div>
-
-                    <div class="col-9 col-md-6 text-right p-0">
-
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-light">
-                                <i class="far fa-image"></i>
-                            </button>
-                            <button type="button" class="btn btn-light">
-                                <i class="fas fa-film"></i>
-                            </button>
-
-                            <button type="button" class="btn btn-light">
-                                <i class="fas fa-music"></i>
-                            </button>
-
-
-                        </div>
-
-                    </div>
-
-
-
-
-
+            <div class="jumbotron p-3 post mb-2">
+              <div class="row">
+                <img src="images/system/dummy_profile.svg" class="col-2 profile_img_chat p-0 border-0">
+                <textarea placeholder="New post..." class="col-4 col-md-7 text-primary form-control-sm border border-secondary mr-0" rows="1" id="new_post_ta" style="height: 30px;"></textarea>
+                <div class="btn-group col-2" role="group" style="height: 100%;">
+                  <button type="button" class="btn btn-light">
+                    <i class="far fa-image"></i>
+                  </button>
+                  <button type="button" class="btn btn-light">
+                    <i class="fas fa-film"></i>
+                  </button>
+                  <button type="button" class="btn btn-light">
+                    <i class="fas fa-music"></i>
+                  </button>
                 </div>
-
-                <div class="row p-2">
-
-                    <div class="col-12">
-
-                        <form class="form-inline row justify-content-between">
-                            <textarea placeholder="" class="col-9 col-sm-9 col-md-9 text-primary form-control-sm border border-secondary" rows="5" id="new_comment_ta"></textarea>
-                            <input type="submit" value="post" class="btn btn-primary btn-sm col-2">
-                        </form>
-
-                    </div>
-
-
-
-                </div>
-
-
-
+              </div>
             </div>
 
             <div class="jumbotron p-3 post mb-2">
