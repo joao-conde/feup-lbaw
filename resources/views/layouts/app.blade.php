e3528d81e667d1455801219377a5cbfc43a9d7bf
João Furriel <joaopedrofump@gmail.com>
2018-04-13 16:31:42 +0100

    first study on laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+lmTKXkS+c9d34U9obDdGOZT7zqFicJDkhckYYsW7oenXR37T2OEV4uqfUO45M87" crossorigin="anonymous">
+  
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script type="text/javascript">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Styles -->
+  <!-- <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet"> -->
+  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+  <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-    </script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer>
-</script>
-  </head>
-  <body>
-    <main>
-      <header>
-        <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
+  </script>
+  <script type="text/javascript" src={{ asset( 'js/app.js') }} defer>
+  </script>
+</head>
+
+<body>
+  <main>
+    <header id="header" class="container-fluid bg-primary text-white">
+      <!-- <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
         @if (Auth::check())
         <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-        @endif
-      </header>
-      <section id="content">
-        @yield('content')
-      </section>
-    </main>
-  </body>
+        @endif -->
+      @include('partials.header')
+    </header>
+    <section id="content">
+      @yield('content')
+    </section>
+  </main>
+</body>
+
 </html>
