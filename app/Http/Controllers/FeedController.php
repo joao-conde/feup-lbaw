961f8826a9d76fb4b486db1c3f701360e1fa785a
joao-conde <joaoconde_13@hotmail.com>
2018-05-21 16:25:40 +0100

    create post in db

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use App\Content;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -29,79 +28,23 @@ class FeedController extends Controller
 	 * @return Post The post created.
 	 */
 	public function createPost(Request $request)
-	{
+	{    
         
-        $result = array();
+        DB::beginTransaction();
+        DB::statement('SET TRANSACTION ISOLATION LEVEL REPEATABLE READ');
 
-        /*
-            BEGIN TRANSACTION;
-            SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
+        $insertContent = "INSERT INTO content (text, creatorId)
+                                VALUES (?, ?)";
 
-            -- Insert content
-            INSERT INTO content (text, creatorId)
-            VALUES ($text, $creatorId);
+        $insertPost = "INSERT INTO post (private, contentId)
+                                VALUES (?, currval('content_id_seq'))";
 
-            -- Insert post
-            INSERT INTO post (private, contentId)
-            VALUES ($private, currval('content_id_seq'));
-
-            COMMIT;
-
-        */
-    
-        $result = DB::transaction(function () {
-            $db_result = array();
-    
-            DB::statement('SET TRANSACTION ISOLATION LEVEL REPEATABLE READ');
-            
-            $insertContent = "INSERT INTO content (text, creatorId)
-            VALUES ($text, $creatorId)";
-
-            $insertPost = "INSERT INTO post (private, contentId)
-            VALUES ($private, currval('content_id_seq'))";
-
-            DB::insert($insertContent, []);
-            
-            /*
-            $db_result['count'] = DB::select($countQuery, [$this->id])[0]->count;
-                
-            $notificationsQuery = $this->queryNotifications();
-    
-            $db_result['notifications'] = DB::select($notificationsQuery, [$this->id]);
-            */
-            return $db_result;
-        });
-    
-        return $result;
+        DB::insert($insertContent, [$request->content, Auth::user()->id]);
+        DB::insert($insertPost, [$request->private]);
         
-
-
-        $content = new Content();
-        $post = new Post();
-        DB::transaction(function() {
-            
-            //$content->id = $request;
-            $content->text = $request->input('content');
-            //$content->date = $request->input();
-            $content->date = "31/02/2022";
-            $content->creatorid = Auth::user()->id;
-            $content->isactive = true;
-            
-            //$content->save();
-
+        DB::commit();
         
-            
-            $post->id = $request->input('id');
-            //$post->private = $request->input('private');
-            
-            //$post->text = $request->input('post');
-            
-            //$post->save();
-            
-        });
-
-
-        return response($post->id, 200);
+        return response(json_encode(['content' => $request->content]), 200);
 	}
 
 }
--- public/js/newPost.js
+++ public/js/newPost.js
@@ -14,18 +14,16 @@ postButton.addEventListener('click', function(){
 
   let request = new XMLHttpRequest();
   let method = POST;
-  let api = '/api/users/{userId}/posts';
+  //TODO: change to user ID
+  let api = '/api/users/' + '20' + '/posts';
   
   let data = {
-    id:   25,
     private: false,
     content: textarea.value
   };
 
-  //console.log("wat");
-  let requestData = encodeForAjax(data);
-  sendAsyncAjaxRequest(request, api, method, handleCreatePostAPIResponse.bind(textarea, request, "post"), URL_ENCODE, data);
-  
+  sendAsyncAjaxRequest(request, api, method, handleCreatePostAPIResponse.bind(textarea, request, "post"), JSON_ENCODE,JSON.stringify(data));
+  postButton.style.display = 'none';
 })
 
 
@@ -34,11 +32,13 @@ textarea.addEventListener('focusout',function(){
 })
 
 
-function handleCreatePostAPIResponse(request, type){
+function handleCreatePostAPIResponse(response, type){
 
-  if(request.status != 200)
+  if(response.status != 200)
     return;
 
+
+  //TODO: create a post with info (request more info)
   let post_div = document.querySelector('#posts');
   let new_post = document.createElement('div');
   new_post.classList.add('post');
@@ -46,10 +46,10 @@ function handleCreatePostAPIResponse(request, type){
 
   let text = document.createElement('textarea');
   new_post.appendChild(text);
-  text.innerHTML = request.responseText;
 
-  console.log("HANDLE AJAX RESPONSE:" + request.responseText);
+  let data = JSON.parse(response.responseText);
+  
 
-  postButton.style.display = 'none';
+  new_post.innerHTML = data.content;
 }
 
