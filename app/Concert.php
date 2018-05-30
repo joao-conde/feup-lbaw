172432cc335aaa4ce73aa2efe471b6bb7a677102
therealdelay <danny.a_soares@hotmail.com>
2018-05-30 04:26:34 +0100

    schedule/remove concerts

--- app/Concert.php
+++ app/Concert.php
@@ -12,12 +12,16 @@ class Concert extends Model
 {
     protected $table = 'concert';
 
+    protected $fillable = [
+        'locationid', 'concertdate', 'description', 'bandid', 'isactive', 'id'
+    ];
+
     // Don't add create and update timestamps in database.
     public $timestamps  = false;
 
 
     public static function getBandConcerts($id){
-        $query = "SELECT concert.description, concert.concertDate, city.name from concert JOIN city ON concert.locationid=city.id WHERE bandid=?";
+        $query = "SELECT concert.id, concert.description, concert.concertDate, city.name from concert JOIN city ON concert.locationid=city.id WHERE bandid=? and concert.isactive = true";
         return DB::select($query, [$id]);
     }
 
--- resources/views/pages/band.blade.php
+++ resources/views/pages/band.blade.php
@@ -60,164 +60,175 @@
                         <div class="row justify-content-center p-2">
                             <div class="col-auto align-self-center">
                                 @if($roundedRate > 0) @for($i = 0; $i
-                                < $wholeRate; $i++) <span class="mt-1 text-primary fullstar">
-                                    <i class="fas fa-star"></i>
+                                    < $wholeRate; $i++) <span class="mt-1 text-primary fullstar">
+                                        <i class="fas fa-star"></i>
                                     </span>
                                     @endfor @if($decimalRate > 0)
                                     <span class="mt-1 text-primary halfstar">
                                         <i class="fas fa-star-half"></i>
                                     </span>
                                     @endif @for($i = $wholeRate; $i
-                                    < 5; $i++) <span class="mt-1 text-primary emptystar">
-                                        <i class="far fa-star"></i>
+                                        < 5; $i++) <span class="mt-1 text-primary emptystar">
+                                            <i class="far fa-star"></i>
                                         </span>
                                         @endfor @endif
 
                                         <small class="m-1 text-info">{{$roundedRate > 0 ? $roundedRate : "Not rated yet"}}</small>
-                            </div>
+                                    </div>
 
-                        </div>
-
-
-
-                        <div id="bios">
-                            <ul class="row p-2 mt-2 justify-content-center">
-                                <li>@if($location != '')
-                                    <i>{{$location}}, {{$country}}</i>
-                                    @endif
-                                </li>
-                                <li>
-                                    <small>Founded on
-                                        <i>{{date('Y',strtotime($band->creationdate))}}</i>
-                                        <p class="d-inline"> by
-                                            <a class="text-primary" href={{ "/users/".$band[ 'founders'][0]->userid}}>{{$band['founders'][0]->membername}}</p>
-                                        </a>
-                                        @if(count($band['founders']) > 2) @for($i = 1; $i
-                                        < count($band[ 'founders'])-1; $i++) , <a class="text-primary" href={ { "/users/".$band[ 'founders'][$i]->userid}}></i> {{$band['founders'][$i]->membername}}</i>
-                                            </a>
-                                            @endfor and
-                                            <a class="text-primary" href={ { "/users/".$band[ 'founders'][count($band[ 'founders'])-1]->userid}}></i> {{$band['founders'][count($band['founders'])-1]->membername}}</a>
-                                            @endif
-                                    </small>
-
-                                </li>
-                            </ul>
-                        </div>
-
-
-
-                    </div>
-
-                </div>
-
-                <hr>
-
-                <div id="navprofile" class="row p2 justify-content-center border-top">
-
-                    <nav class="navbar navbar-expand navbar-light bg-light">
-
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav">
-                                <a class="nav-item col-6 nav-link btn btn-secondary text-white mr-2" href="#">Members</a>
-                                <a class="nav-item col-6 nav-link btn btn-secondary text-white mr-2" href="#">Followers</a>
-                            </div>
-                        </div>
-                    </nav>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-    <div class="row no-gutters">
-        <div class="col-3 d-none d-lg-block">
-
-            <div id="members" class="jumbotron p-3 mr-2">
-                <h4>Members</h4> @foreach($members as $member)
-
-                <div class="d-block">
-
-                    <img class="profile_img_feed" src="{{User::getUserIconPicturePath($member->userid)}}">
-                    <a href="{{'/users/'.$member->userid}}">
-                        <small class="text-primary">{{$member->membername}}</small>
-                    </a>
-
-                    @if($member->owner == true)
-                    <small class="ml-1">f</small>
-                    @endif @if($member->pending == true)
-                    <small class="ml-1">p</small>
-                    @endif @if($isFounder && !$member->owner) @if($member->pending == true)
-                    <span class="col-2 clickable remove_invite_button">
-                        <span id="userId" class="d-none">{{$member->userid}}</span>
-                        <i class="fas fa-times text-danger"></i>
-                    </span>
-                    @endif @if($member->pending == false)
-                    <span class="col-2 clickable remove_member_button">
-                        <span id="memberId" class="d-none">{{$member->userid}}</span>
-                        <i class="fas fa-times text-danger"></i>
-                    </span>
-                    @endif @endif
-
-                </div>
-
-
-                @endforeach @if($isFounder)
-
-                <div class="mt-3 autocomplete" id="inviteMember">
-                    <input id="band_members" placeholder="Invite member..." type="text" name="name">
-                </div>
-                <div class="collapse show" id="new_members">
-                </div>
-
-                @endif
-
-                <hr>
-                <div id="concerts">
-                    <h4>Scheduled Concerts</h4> @foreach($concerts as $concert) @include('partials.concert',[$concert, $band]) @endforeach
-
-                    
-                    <h5 class="m-0 mt-3 text-primary">New concert</h5>
-                    <div class="new_concert d-block">
-                        <input name="description" placeholder="Short description"></input>
-                        <div class="btn-group edit_field">
-                                <button type="button" class="btn btn-sm btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Edit Location
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-left col-12 p-3 bg-secondary">
-                                    @foreach($cities as $city)
-                                        @include('partials.location')
-                                    @endforeach
-                                    
                                 </div>
+
+
+
+                                <div id="bios">
+                                    <ul class="row p-2 mt-2 justify-content-center">
+                                        <li>@if($location != '')
+                                            <i>{{$location}}, {{$country}}</i>
+                                            @endif
+                                        </li>
+                                        <li>
+                                            <small>Founded on
+                                                <i>{{date('Y',strtotime($band->creationdate))}}</i>
+                                                <p class="d-inline"> by
+                                                    <a class="text-primary" href={{ "/users/".$band[ 'founders'][0]->userid}}>{{$band['founders'][0]->membername}}</p>
+                                                    </a>
+                                                    @if(count($band['founders']) > 2) @for($i = 1; $i
+                                                        < count($band[ 'founders'])-1; $i++) , <a class="text-primary" href={ { "/users/".$band[ 'founders'][$i]->userid}}></i> {{$band['founders'][$i]->membername}}</i>
+                                                        </a>
+                                                        @endfor and
+                                                        <a class="text-primary" href={ { "/users/".$band[ 'founders'][count($band[ 'founders'])-1]->userid}}></i> {{$band['founders'][count($band['founders'])-1]->membername}}</a>
+                                                        @endif
+                                                    </small>
+
+                                                </li>
+                                            </ul>
+                                        </div>
+
+
+
+                                    </div>
+
+                                </div>
+
+                                <hr>
+
+                                <div id="navprofile" class="row p2 justify-content-center border-top">
+
+                                    <nav class="navbar navbar-expand navbar-light bg-light">
+
+                                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                            <div class="navbar-nav">
+                                                <a class="nav-item col-6 nav-link btn btn-secondary text-white mr-2" href="#">Members</a>
+                                                <a class="nav-item col-6 nav-link btn btn-secondary text-white mr-2" href="#">Followers</a>
+                                            </div>
+                                        </div>
+                                    </nav>
+
+                                </div>
+
+                            </div>
+
                         </div>
-                        <input name="date" type="date" placeholder="Date"></input>
+
                     </div>
+
+                    <div class="row no-gutters">
+                        <div class="col-3 d-none d-lg-block">
+
+                            <div id="members" class="jumbotron p-3 mr-2">
+                                <h4>Members</h4> @foreach($members as $member)
+
+                                <div class="d-block">
+
+                                    <img class="profile_img_feed" src="{{User::getUserIconPicturePath($member->userid)}}">
+                                    <a href="{{'/users/'.$member->userid}}">
+                                        <small class="text-primary">{{$member->membername}}</small>
+                                    </a>
+
+                                    @if($member->owner == true)
+                                    <small class="ml-1">f</small>
+                                    @endif @if($member->pending == true)
+                                    <small class="ml-1">p</small>
+                                    @endif @if($isFounder && !$member->owner) @if($member->pending == true)
+                                    <span class="col-2 clickable remove_invite_button">
+                                        <span id="userId" class="d-none">{{$member->userid}}</span>
+                                        <i class="fas fa-times text-danger"></i>
+                                    </span>
+                                    @endif @if($member->pending == false)
+                                    <span class="col-2 clickable remove_member_button">
+                                        <span id="memberId" class="d-none">{{$member->userid}}</span>
+                                        <i class="fas fa-times text-danger"></i>
+                                    </span>
+
+                                    @endif @endif
+
+                                </div>
+
+
+                                @endforeach @if($isFounder)
+
+                                <div class="mt-3 autocomplete" id="inviteMember">
+                                    <input id="band_members" placeholder="Invite member..." type="text" name="name">
+                                </div>
+                                <div class="collapse show" id="new_members">
+                                </div>
+
+                                @endif
+
+                                <hr>
+                                <div id="concerts">
+                                    <h4>Scheduled Concerts</h4>
+                                    <div id="scheduled_concerts">
+                                        @foreach($concerts as $concert) 
+                                        @include('partials.concert',[$concert]) 
+                                        @endforeach
+                                    </div>
+
+
+                                    <h5 class="m-0 mt-3 text-primary">New concert</h5>
+
+                                    <div class="text-left">
+
+                                        <div class="new_concert d-block">
+                                            <input type="text" id="description" name="description" class="mt-1 w-75" placeholder="Short description"></input>
+                                            <input id="date" name="date" type="date" class="mt-1 w-75" placeholder="Date" required></input><br>
+                                            <i id="location" class="mt-1 w-75">Location</i><br>
+                                            <button type="button" class="mt-1 btn btn-sm btn-primary w-75 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" required>
+                                                Edit Location
+                                            </button>
+                                            <span class="d-none" id="location_id"></span>
+                                            <div class="dropdown-menu dropdown-menu-left p-3 bg-secondary">
+                                                @foreach($cities as $city)
+                                                @include('partials.location')
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                        <button type="button" class="btn btn-primary mt-1 w-50" id="schedule_button">Schedule</button>
+                                        <br><p class="" id="schedule_error" style="display: none;">Error: Invalid concert scheduling.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-12 col-md-9 col-lg-6">
+
+                            <div id="center_content" class="toggleContent">
+
+                                @if($band->isMember(Auth::user()->id))
+                                <?php $bandNewPost = true ?> @include('partials.new_post',[$bandNewPost]) @endif
+
+                                <div id="posts">
+                                    @foreach($band['posts'] as $post) @include('partials.post') @endforeach
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
                 </div>
 
-            </div>
-
-        </div>
-
-        <div class="col-12 col-md-9 col-lg-6">
-
-            <div id="center_content" class="toggleContent">
-
-                @if($band->isMember(Auth::user()->id))
-                <?php $bandNewPost = true ?> @include('partials.new_post',[$bandNewPost]) @endif
-
-                <div id="posts">
-                    @foreach($band['posts'] as $post) @include('partials.post') @endforeach
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
-
-@endsection
+                @endsection
